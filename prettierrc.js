ae5e324c717e59722f4d363509804acbcc5b9547
chimemoo <christ.16062@student.unsika.ac.id>
2021-07-20 12:57:26 +0700

    add eslint & prettier rule

---
+++ prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
